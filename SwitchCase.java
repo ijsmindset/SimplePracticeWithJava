cdbe13452ccff3fe104ec41646157d6d18484855
ijDev <184642607+ijsmindset@users.noreply.github.com>
2025-02-08 17:17:46 +0100

    Update SwitchCase.java

--- SwitchCase.java
+++ SwitchCase.java
@@ -2,7 +2,7 @@ public class SwitchCase{
    public static void main(String args[]){
   
   int num_uno = 5, num_dos = 3, resultado = 0;
-  int parametro = 2;
+  int parametro = 4;
   
   switch(parametro){
 
