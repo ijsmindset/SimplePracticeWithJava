2d2d7093c482d9fcbce9d600c9940bde78a279c1
ijDev <184642607+ijsmindset@users.noreply.github.com>
2025-05-10 08:31:11 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,3 @@ public class HelloWorld{
   }
 }
 /*mi primer hola mundo esperemos que sea el inicio de un nuevo  */
-public class HelloWorld{
-  public static void main(String args[]){
-    System.out.println("hello world");
-  }
-}
-/*mi primer hola mundo esperemos que sea el inicio de un nuevo  */
-public class HelloWorld{
-  public static void main(String args[]){
-    System.out.println("hello world");
-  }
-}
-/*mi primer hola mundo esperemos que sea el inicio de un nuevo  */
