c8ceb66178c87bdad884e0a5158473409b3155b9
ijDev <184642607+ijsmindset@users.noreply.github.com>
2025-05-14 09:31:06 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String args[]){
-    System.out.println("hello world " "zona de pruebas para java aqui realizaremo la impresion de salida output ");
+    System.out.println("hello world " "zona");
   }
 }
