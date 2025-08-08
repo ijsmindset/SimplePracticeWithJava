e1a048ecc868f48cfee6e145dd0d2c743feef1c0
ijDev <184642607+ijsmindset@users.noreply.github.com>
2025-08-08 10:09:07 +0200

    Actualizar Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Suma{
   public static void main(String args[]){
 
    int num_uno   = 5;
-   int num_dos   = 11;
+   int num_dos   = 3;
    int resultado = 0;
 
    resultado = num_uno + num_dos;
